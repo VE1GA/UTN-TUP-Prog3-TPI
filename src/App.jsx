c2e72d8c553d22cce38b0d290e6a06a0b0735f02
Facundo Veiga <59349743+VE1GA@users.noreply.github.com>
2025-04-15 00:10:07 -0300

    Importación de un proyecto básico anterior
    Se copió un proyecto estable realizado en clases donde se trabajó el uso de states y se hicieron muy pocas modificaciones en "package-lock.json" y "package.json".
    
    La idea es usar este proyecto como base y a partir de acá modificarlo acorde a lo que haga falta.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import CustomComponent from "./components/CustomComponent/CustomComponent";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <CustomComponent></CustomComponent>
+    </>
+  );
+}
+
+export default App;
