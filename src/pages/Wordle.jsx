fffbac25a82376ff885b1d07ed6557a4badf4860
Facundo Veiga <59349743+VE1GA@users.noreply.github.com>
2025-04-30 16:55:24 -0300

    [+] Keyboard
    Se añadieron las funciones respectivas al teclado:
    - El renderizado de las letras en el tablero
    - Las funciones de las teclas Enter y Delete
    
    - Se empezó a desarrollar la lógica interna del juego

--- src/pages/Wordle.jsx
+++ src/pages/Wordle.jsx
@@ -1,19 +1,57 @@
 import React from "react";
+import { createContext, useState } from "react";
+
 import Board from "../components/Board";
 import Keyboard from "../components/Keyboard";
-import "../App.css";
-import { createContext, useState } from "react";
 import { boardDefault } from "../components/Words";
 
 export const WordleContext = createContext();
 
 const Wordle = () => {
+  // Estados
   const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0 });
+
+  // Funciones
+  const onSelectLetter = (keyVal) => {
+    if (currAttempt.letterPos > 4) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
+    setBoard(newBoard);
+    setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos + 1 });
+  };
+
+  const onEnter = () => {
+    if (currAttempt.letterPos !== 5) return; // Si todavía no se completó el renglón, no es posible apretar "Enter"
+    setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 }); // Pasa al siguiente renglón
+  };
+
+  const onDelete = () => {
+    if (currAttempt.letterPos === 0) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
+    setBoard(newBoard);
+    setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos - 1 });
+  };
+
+  // Renderizado
   return (
     <div>
-      <WordleContext.Provider value={{ board, setBoard }}>
-        <Board />
-        <Keyboard />
+      <WordleContext.Provider
+        value={{
+          board,
+          setBoard,
+          currAttempt,
+          setCurrAttempt,
+          onSelectLetter,
+          onEnter,
+          onDelete,
+        }}
+      >
+        <div className="game">
+          <Board />
+          <Keyboard />
+        </div>
       </WordleContext.Provider>
     </div>
   );
