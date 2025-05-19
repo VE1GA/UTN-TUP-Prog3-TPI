d35e314cd616a876a8a714879335c8c878b96cdd
Fausto Ponce <Faustoponceb@gmail.com>
2025-05-19 10:42:48 -0300

    creacion de la carpeta front

---
+++ Front/README.md
@@ -0,0 +1,2 @@
+# UTN-TUP-Prog3-TPI
+ Trabajo Práctico Integrador de la materia "Programación 3" de la carrera "Tecnicatura Universitaria en Programación" de la UTN Rosario, cursada en 2025.
---
+++ Front/src/components/Words.jsx
@@ -0,0 +1,24 @@
+export const boardDefault = [
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
+
+export const wordArray = [
+  "papel",
+  "perro",
+  "llave",
+  "mundo",
+  "brazo",
+  "trigo",
+  "mujer",
+  "calle",
+  "raton",
+  "reloj",
+  "rouli",
+  "rawrr",
+  "roulio",
+];
