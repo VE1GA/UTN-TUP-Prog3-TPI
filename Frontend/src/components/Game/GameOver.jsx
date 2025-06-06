94a2901813691de4051009a240e1accbe59151d6
mateeoz <mateogithub06@gmail.com>
2025-06-06 09:10:51 -0300

    Modal de GameOver + Toast de palabra no encontrada

--- Frontend/src/components/Game/GameOver.jsx
+++ Frontend/src/components/Game/GameOver.jsx
@@ -7,8 +7,8 @@ const GameOver = () => {
 
   return (
     <div className="gameOver">
-      <h3>{gameOver.guessedWord ? "Adivinaste la palabra" : "Perdiste"}</h3>
-      <h1>Palabra correcta: {correctWord}</h1>
+      <h1>{gameOver.guessedWord ? "Adivinaste la palabra" : "Perdiste"}</h1>
+      <h2>Palabra correcta: {correctWord}</h2>
       {gameOver.guessedWord && (
         <h3>Adivinaste en: {currAttempt.attempt} intentos</h3>
       )}
